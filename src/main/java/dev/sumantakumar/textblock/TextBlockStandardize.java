5d95a0c91f4582f79fb1789397650a4d5ca45f4f
Sumanta Kumar Sahoo <sumant7654@gmail.com>
2025-02-09 07:18:29 -0800

    text block standardized

---
+++ src/main/java/dev/sumantakumar/textblock/TextBlockStandardize.java
@@ -0,0 +1,18 @@
+package dev.sumantakumar.textblock;
+
+public class TextBlockStandardize {
+    public static void main(String[] args) {
+        String withoutTextBlock = "Hello World!," +
+                "\nThis is a Simple String." +
+                "\nPlease use Text Block";
+
+        System.out.println(withoutTextBlock);
+        System.out.println("=================");
+        String withTextBlock = """
+                Hello World!,
+                This is a standardized text block.
+                Thanks for using Text Block!""";
+
+        System.out.println(withTextBlock);
+    }
+}
