962d7e8d40d13d2c9c6ebc2a95cfdb4c1bf69001
Sumanta Kumar Sahoo <sumant7654@gmail.com>
2025-02-09 07:11:03 -0800

    isEmpty() default method added in CharSequence class

---
+++ src/main/java/dev/sumantakumar/charsequence/IsEmpty.java
@@ -0,0 +1,11 @@
+package dev.sumantakumar.charsequence;
+
+public class IsEmpty {
+    public static void main(String[] args) {
+        CharSequence charSeq = new StringBuilder();
+        if (charSeq.isEmpty()) {
+            System.out.println("The CharSequence is empty!");
+        }
+
+    }
+}
