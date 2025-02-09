5a5f28d8db470337957f72e7c870e82a76d2ae2b
Sumanta Kumar Sahoo <sumant7654@gmail.com>
2025-02-09 09:00:23 -0800

    text block standardized

--- src/main/java/dev/sumantakumar/textblock/TextBlockStandardize.java
+++ src/main/java/dev/sumantakumar/textblock/TextBlockStandardize.java
@@ -4,14 +4,15 @@ public class TextBlockStandardize {
     public static void main(String[] args) {
         String withoutTextBlock = "Hello World!," +
                 "\nThis is a Simple String." +
-                "\nPlease use Text Block";
+                "\nPlease use \"Text Block!\"";
 
         System.out.println(withoutTextBlock);
         System.out.println("=================");
         String withTextBlock = """
                 Hello World!,
-                This is a standardized text block.
-                Thanks for using Text Block!""";
+                This is a q text block.
+                Thanks for using "Text Block!"
+                """;
 
         System.out.println(withTextBlock);
     }
